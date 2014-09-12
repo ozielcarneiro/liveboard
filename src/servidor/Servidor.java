daaacebc14961629035bbaf7dd65631a1ead31ba
Oziel Carneiro <ozielcarneiro@gmail.com>
2014-09-11 23:53:34 -0700

    project first upload to git

---
+++ src/servidor/Servidor.java
@@ -0,0 +1,55 @@
+package servidor;
+
+import entity.Painel;
+import java.awt.Graphics;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author oziel
+ */
+public class Servidor {
+
+    private ServerSocket server;
+    private Socket client;
+    private ArrayList<ConnectionHandler> list;
+
+    public void start() {
+        list = new ArrayList<ConnectionHandler>();
+        try {
+            server = new ServerSocket(1234);
+            while (true) {
+                System.out.println("Servidor em espera...");
+                client = server.accept();
+                System.out.println(client.getInetAddress() + " conectado.");
+                list.add(new ConnectionHandler(this, client));
+                list.get(list.size()-1).start();
+                
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void msgReceived(String msg, ConnectionHandler sender) {
+        for (int i = 0; i < list.size(); i++) {
+            if (!list.get(i).equals(sender)) {
+                list.get(i).sendMsg(msg);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Servidor s = new Servidor();
+        s.start();
+    }
+}
